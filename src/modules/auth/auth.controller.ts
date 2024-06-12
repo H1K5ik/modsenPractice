0738d65b56f5237f9539000bef7314405c0627e2
H1K5ik <nikikra2323@gmail.com>
2024-06-12 15:06:54 +0300

    feat: add swagger in auth module

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -11,22 +11,47 @@ import { AuthService } from './auth.service';
 import { Request, Response } from 'express';
 import { JwtAuthGuard } from './guards/jwt-auth.guard';
 import { AuthDto } from './dto/auth.dto';
+import { ApiBody, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('auth')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @ApiResponse({
+    status: 201,
+    description: 'The record has been successfully created.',
+  })
+  @ApiResponse({ status: 400, description: 'Bad request.' })
+  @ApiBody({
+    type: AuthDto,
+    description: 'Json structure for user object',
+  })
   @Post('register')
   async register(@Body() dto: AuthDto) {
     return await this.authService.register(dto);
   }
 
+  @ApiResponse({
+    status: 201,
+    description: 'User loged succesfully',
+  })
+  @ApiResponse({ status: 400, description: 'Bad request.' })
+  @ApiBody({
+    type: AuthDto,
+    description: 'Json structure for user object',
+  })
   @Post('login')
   async login(@Body() dto: AuthDto, @Res({ passthrough: true }) res: Response) {
     const access_token = await this.authService.login(dto);
     res.cookie('Token', access_token.access_token);
   }
 
+  @ApiResponse({
+    status: 201,
+    description: 'User loged succesfully',
+  })
+  @ApiResponse({ status: 400, description: 'Bad request.' })
   @UseGuards(JwtAuthGuard)
   @Get('update-tokens')
   update(@Req() req: Request) {
--- src/modules/auth/dto/payload.dto.ts
+++ src/modules/auth/dto/payload.dto.ts
@@ -1,13 +1,26 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class PayloadDto {
+  @ApiProperty({
+    example: 'id',
+    required: true,
+  })
   @IsNotEmpty()
-  sub: string;
+  sub: number;
 
+  @ApiProperty({
+    example: 'test@mail.ru',
+    required: true,
+  })
   @IsNotEmpty()
   @IsEmail()
   email: string;
 
+  @ApiProperty({
+    example: 'Admin or user',
+    required: true,
+  })
   @IsNotEmpty()
   @IsString()
   roles: string;
