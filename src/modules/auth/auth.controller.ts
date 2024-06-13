83f3d626165614a53a7127f4906f35dd679e4072
H1K5ik <nikikra2323@gmail.com>
2024-06-13 16:02:48 +0300

    feat: add config.ts && refactor: auth && meetup controller

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -5,55 +5,33 @@ import {
   Post,
   Req,
   Res,
-  UseGuards,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { Request, Response } from 'express';
-import { JwtAuthGuard } from './guards/jwt-auth.guard';
 import { AuthDto } from './dto/auth.dto';
-import { ApiBody, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { ApiTags } from '@nestjs/swagger';
 import { UserDto } from './dto/user.dto';
+import { ApiResponseAndBody } from '../../libs/config/config';
 
 @ApiTags('auth')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @ApiResponse({
-    status: 201,
-    description: 'The record has been successfully created.',
-  })
-  @ApiResponse({ status: 400, description: 'Bad request.' })
-  @ApiBody({
-    type: AuthDto,
-    description: 'Json structure for user object',
-  })
+  @ApiResponseAndBody('register')
   @Post('register')
   async register(@Body() dto: AuthDto): Promise<UserDto> {
     return await this.authService.register(dto);
   }
 
-  @ApiResponse({
-    status: 201,
-    description: 'User loged succesfully',
-  })
-  @ApiResponse({ status: 400, description: 'Bad request.' })
-  @ApiBody({
-    type: AuthDto,
-    description: 'Json structure for user object',
-  })
+  @ApiResponseAndBody('login')
   @Post('login')
   async login(@Body() dto: AuthDto, @Res({ passthrough: true }) res: Response) {
     const access_token = await this.authService.login(dto);
     res.cookie('Token', access_token);
   }
 
-  @ApiResponse({
-    status: 201,
-    description: 'User loged succesfully',
-  })
-  @ApiResponse({ status: 400, description: 'Bad request.' })
-  @UseGuards(JwtAuthGuard)
+  @ApiResponseAndBody('update-tokens')
   @Get('update-tokens')
   update(@Req() req: Request) {
     return this.authService.updateTokens(req.cookies.Token);
--- src/modules/meetup/meetup.controller.ts
+++ src/modules/meetup/meetup.controller.ts
@@ -7,72 +7,40 @@ import {
   Patch,
   Post,
   Query,
-  UseGuards,
 } from '@nestjs/common';
 import { MeetupService } from './meetup.service';
 import { MeetupDto } from './dto/meetup.dto';
 import { QueryDto } from './dto/query.dto';
-import { ApiBody, ApiResponse, ApiTags } from '@nestjs/swagger';
-import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 import { GetUserId } from 'src/libs/decorators/userid.decorator';
-import { RolesGuard } from '../auth/guards/roles.guard';
-import { Roles } from 'src/libs/decorators/roles.decorator';
+import { ApiResponseAndBody } from '../../libs/config/config';
 
-@ApiTags('meetup')
+@ApiResponseAndBody('meetup')
 @Controller('meetup')
-@UseGuards(JwtAuthGuard)
 export class MeetupController {
   constructor(private readonly meetupService: MeetupService) {}
 
-  @ApiResponse({
-    status: 200,
-    description: 'Meetups has been successfully created. found found',
-  })
-  @ApiResponse({ status: 404, description: 'Not Found' })
+  @ApiResponseAndBody('getAllMeetups')
   @Get()
   async getAllMeetups(@Query() query: QueryDto): Promise<MeetupDto[]> {
     return await this.meetupService.getAllMeetups(query);
   }
 
-  @ApiResponse({
-    status: 200,
-    description: 'The meetup has been successfully created. found',
-  })
-  @ApiResponse({ status: 404, description: 'Not Found' })
+  @ApiResponseAndBody('getMeetupById')
   @Get(':id')
-  async getMeeupById(@Param('id') id: number): Promise<MeetupDto> {
+  async getMeetupById(@Param('id') id: number): Promise<MeetupDto> {
     return await this.meetupService.getMeeupById(id);
   }
 
-  @ApiResponse({
-    status: 201,
-    description: 'The meetup has been successfully created.',
-  })
-  @ApiResponse({ status: 400, description: 'Bad request' })
-  @ApiBody({
-    type: MeetupDto,
-    description: 'Json structure for user object',
-  })
+  @ApiResponseAndBody('createMeetup')
   @Post('create')
-  async ceateMeetup(
+  async createMeetup(
     @GetUserId('id') id: number,
     @Body() dto: MeetupDto,
   ): Promise<MeetupDto> {
     return await this.meetupService.createMeetup(id, dto);
   }
 
-  @ApiResponse({
-    status: 200,
-    description: 'The meetup has been successfully changed.',
-  })
-  @ApiResponse({ status: 403, description: 'Forbidden' })
-  @ApiResponse({ status: 404, description: 'Not Found' })
-  @ApiBody({
-    type: MeetupDto,
-    description: 'Json structure for user object',
-  })
-  @UseGuards(RolesGuard)
-  @Roles('Admin')
+  @ApiResponseAndBody('changeMeetup')
   @Patch('change/:id')
   async changeMeetup(
     @GetUserId('id') userId: number,
@@ -82,12 +50,7 @@ export class MeetupController {
     return await this.meetupService.changeMeetup(userId, id, dto);
   }
 
-  @ApiResponse({
-    status: 200,
-    description: 'The meetup has been successfully deleted.',
-  })
-  @ApiResponse({ status: 403, description: 'Forbidden' })
-  @ApiResponse({ status: 404, description: 'Not found' })
+  @ApiResponseAndBody('deleteMeetup')
   @Delete('delete/:id')
   async deleteMeetup(
     @GetUserId('id') userId: number,
