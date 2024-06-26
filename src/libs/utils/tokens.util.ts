27d8c03ec6db9e787b0982e8e8ce92382d50b9ce
H1K5ik <nikikra2323@gmail.com>
2024-06-26 17:48:36 +0300

    refactor: add hash refreshToken

--- src/libs/utils/tokens.util.ts
+++ src/libs/utils/tokens.util.ts
@@ -1,27 +1,29 @@
 import { JwtService } from '@nestjs/jwt';
 import { PrismaClient } from '@prisma/client';
+import * as bcrypt from 'bcrypt';
 
-import { AuthDto, PayloadDto } from '@dto';
+import { saltOrRounds } from '@constants';
+import { PayloadDto } from '@dto';
 import { tokenProps } from '@interfaces';
 
 const jwtService = new JwtService();
 const prisma = new PrismaClient();
 export async function createTokens(
   payload: PayloadDto,
-  user: AuthDto,
+  userEmail: string,
 ): Promise<tokenProps> {
   const refreshToken = jwtService.sign(payload, {
     secret: process.env.JWT_SECRET,
     expiresIn: process.env.JWT_REF_EXP,
   });
-
+  const hash = await bcrypt.hash(refreshToken, saltOrRounds);
   await prisma.user.update({
-    where: { email: user.email },
-    data: { Token: refreshToken },
+    where: { email: userEmail },
+    data: { Token: hash },
   });
   const accessToken = jwtService.sign(payload, {
     secret: process.env.JWT_SECRET,
     expiresIn: process.env.JWT_AC_EXP,
   });
-  return { accessToken, refreshToken };
+  return { accessToken, refreshToken: hash };
 }
